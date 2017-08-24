3b38f671dd2ff98ec34d6bf067c6c5f95cb29ce5
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-08-24 17:31:03 -0400

    finished CRM_very_new

--- crm.rb
+++ crm.rb
@@ -5,3 +5,20 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+get '/contacts' do
+  @contacts=Contact.all
+  erb :contacts
+end
+
+get '/' do
+  redirect to('contacts')
+end
+
+get '/about' do
+  erb :about
+end
+
+after do
+  ActiveRecord::Base.connection.close
+end
