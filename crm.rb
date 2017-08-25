d3f54c95913ac58ce49293a644453bad2df6194d
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-08-25 16:47:29 -0400

    edit contact done

--- crm.rb
+++ crm.rb
@@ -7,7 +7,7 @@ get '/' do
 end
 
 
-get '/contacts/' do
+get '/contacts' do
   @contacts=Contact.all
   erb :contacts
 end
@@ -26,11 +26,44 @@ get '/contacts/:id' do
   end
 end
 
+post '/contacts' do
+  Contact.create(
+  first_name: params[:first_name],
+  last_name: params[:last_name],
+  email: params[:email],
+  note: params[:note]
+  )
+  redirect to('/contacts')
+end
+
+get '/contacts/:id/edit' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
 get '/about' do
   erb :about
 end
 
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
 
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
 
 after do
   ActiveRecord::Base.connection.close
